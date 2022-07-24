1a8e1e3b83c01c5027d1e56616336498d0d3e1c2
Dan Farino <dan.farino@eyecuelab.com>
2022-07-24 12:57:43 -0700

    use renamed script, disable echo

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -4,12 +4,11 @@ set -euo pipefail
 
 unset GITHUB_TOKEN
 read -r -s -p "GitHub token: " token
-echo "${token//?/*}"
 echo "$token" | gh auth login --git-protocol https --with-token
 gh auth setup-git
 cd
 gh repo clone danfarino/dotfiles
 rm .gitconfig
-./dotfiles/install
+./dotfiles/setup.sh
 
 rm -f ~/install-dotfiles.sh
