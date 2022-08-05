0a999e881584a3fcc78887306bf18d4ca082a491
Dan Farino <dan.farino@eyecuelab.com>
2022-08-05 16:31:52 -0700

    add ssh key

--- install.sh
+++ install.sh
@@ -5,3 +5,7 @@ set -euo pipefail
 script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
 cp "$script_dir/install-dotfiles.sh" ~
+
+mkdir -p ~/.ssh
+echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDCtd2SeU4ttb65pK58zJNkE7QMI99zCWD0odIbskaxxwtC+NT89E2HaEei+nsNSMKhU3XdANq4X0fjcIhJlkPH/Q+xSyUZ4V6rrbP0H1MuMyBMHyzS7hD4N52wCr3bbgUonRTYSE5pm6bSgRnSJyLt/4ozrl+RLTLoMamllCA2qc/3V54uVN33lPdTYePk5CQxYNZ35uMP66w5upN2UMJEwWYAZNerCAO0hdgU2yzi1lm5tgrsB6kO7MMAPrfAMq4xR+hLvEU7tWDro3F9D8p2wQf+Rsk8elwFWCwNh8oZtqXBbws0dzdUIzDwTtOJtZKUTStWrPssMCpf3lnnADG478xVISuP7k0ZADtn//GCMUV5RSTt83hfEKb1w726Nokd3EW9X0FDFR48rpyrl19vh26vWcfyxo0PodDtWjgKmiue5gRW7k5X/EnYaRQ50CNgDWjkRxvpvohEY5rsTMWzjPiqiFMTxva7GiVIXBZ9uv2KldcYBs6w6jd2exRVqkeW5mR7rWQ8ufJiceaDWIKW3o0mCEKYMHeDQXUaD2pEN6iJ3ALjCJm1AuiF8XjWdTVIWlZqAh9R9A7jzP2pE4rrWD4dzaEGdP8lFoCsQ3o0vCL80lh/6W7cEjjL+/yYMzGji3i7DKrjPQYvguPi7WcxdxGVEfHa1YyYkFsBzQKZOw== danfarino@Dans-MacBook-Pro.local' > ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
